ce4d9abaa26677b4ab5608594b8356385ef8f69a
ZC128 <ZC128@live.mdx.ac.uk>
2018-08-31 01:21:33 +0200

    Add BkgShowWarning.cs

---
+++ Model/GraphicsMessages/BkgShowWarning.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfDestructCommons.Model.GraphicsMessages
+{
+    [Serializable]
+    public class BkgShowWarning : BackgroundCtrlMsg
+    {
+        public BkgShowWarning() : base(BKG_ACTION.SHW_WRN)
+        {
+        }
+    }
+}
